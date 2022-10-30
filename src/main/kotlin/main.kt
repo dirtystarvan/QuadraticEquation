1e9304a8f55ddcea5eb66261b0fcac6635f733f5
Ivan Shishkin <ianshishkin@sberbank.ru>
2022-10-30 20:17:32 +0300

    cleanup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,2 @@
-import kotlin.math.pow
-
 fun main() {
-    println("Here is: ${0.001.pow(2) - 4 * 0.00001 * -0.00001}")
-
-    val solver = SolveEq()
-    solver.epsilon = 0.00001
-
-    println(solver.solve(0.00001, 0.001, -0.00001))
 }
